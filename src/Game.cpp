29f7b81bb89ab9e91f5257cdff77bf2eefa96283
fiflak11 <273060@student.pwr.edu.pl>
2024-06-01 14:34:22 +0200

    clear main loop

--- src/Game.cpp
+++ src/Game.cpp
@@ -5,31 +5,41 @@
 #include "Player/Snake.h"
 #include <memory>
 
+void Game::start() {
+    while (window.isOpen())
+    {
+        events();
+        playerMove();
+        draw();
+    }
+}
+
 Game::Game() {
     window.create(sf::VideoMode(settings.displayResolution[0].first, settings.displayResolution[0].second), "Invaders");
     window.setFramerateLimit(60);
+    player = std::make_unique<LaserMan>(settings.shipSize,settings.shipPosition,20,2,2,2);
 }
 
-void Game::start() {
-    std::unique_ptr<Player> player = std::make_unique<LaserMan>(settings.shipSize,settings.shipPosition,20,2,2,2);
-    while (window.isOpen())
+void Game::playerMove() {
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::D))
+        player->move('r',settings.displayResolution[0].first);
+    else if (sf::Keyboard::isKeyPressed(sf::Keyboard::A))
+        player->move('l',settings.displayResolution[0].first);
+    else
+        player->move('n',settings.displayResolution[0].first);
+}
+
+void Game::events() {
+    sf::Event event;
+    while (window.pollEvent(event))
     {
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            if (event.type == sf::Event::Closed)
-                window.close();
-        }
-
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::D))
-            player->move('r',settings.displayResolution[0].first);
-        else if (sf::Keyboard::isKeyPressed(sf::Keyboard::A))
-            player->move('l',settings.displayResolution[0].first);
-        else
-            player->move('n',settings.displayResolution[0].first);
-
-        window.clear();
-        window.draw(player->getRect());
-        window.display();
+        if (event.type == sf::Event::Closed)
+            window.close();
     }
+}
+
+void Game::draw(){
+    window.clear();
+    window.draw(player->getRect());
+    window.display();
 }
--- src/Game.h
+++ src/Game.h
@@ -3,10 +3,15 @@
 
 #include <SFML/Graphics.hpp>
 #include "Settings.h"
+#include "Player/Player.h"
 
 class Game {
     sf::RenderWindow window;
     const Settings settings;
+    std::unique_ptr<Player> player;
+    void playerMove();
+    void events();
+    void draw();
     public:
     Game();
     void start();
